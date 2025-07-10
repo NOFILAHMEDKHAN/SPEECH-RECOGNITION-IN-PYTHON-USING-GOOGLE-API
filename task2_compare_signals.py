610bb1ddc40af5120da0fdd0a7cfee6a04cf9e99
Nofil ahmed khan <165171833+NOFILAHMEDKHAN@users.noreply.github.com>
2025-07-10 19:55:08 +0500

    Create task2_compare_signals.py

---
+++ task2_compare_signals.py
@@ -0,0 +1,65 @@
+#1:FOR AUDIO_CAR SOUND:
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+frequency_sampling, audio_signal = wavfile.read("C:/Users/PMLS/Downloads/Audio_car.wav")
+print('signal shape:', audio_signal.shape)
+print('signal Datatype:', audio_signal.dtype)
+print('signal Duration:', round(audio_signal.shape[0]/float (frequency_sampling),2),'seconds')
+audio_signal=audio_signal /np.power(2,15)
+audio_signal= audio_signal[:100]
+time_axis=1000*np.arange(0,len(audio_signal),1) / float(frequency_sampling)
+plt.plot(time_axis,audio_signal,color='yellow')
+plt.xlabel('Time(miliseconds)')
+plt.ylabel('Amplitude')
+plt.title('Input audio signal')
+plt.show()
+#2:FOR CAR_ENGINE SOUND:
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+frequency_sampling, audio_signal = wavfile.read("C:/Users/PMLS/Downloads/Audio_carengine.wav")
+print('signal shape:', audio_signal.shape)
+print('signal Datatype:', audio_signal.dtype)
+print('signal Duration:', round(audio_signal.shape[0]/float (frequency_sampling),2),'seconds')
+audio_signal=audio_signal /np.power(2,15)
+audio_signal= audio_signal[:100]
+time_axis=1000*np.arange(0,len(audio_signal),1) / float(frequency_sampling)
+plt.plot(time_axis,audio_signal,color='yellow')
+plt.xlabel('Time(miliseconds)')
+plt.ylabel('Amplitude')
+plt.title('Input audio signal')
+plt.show()
+#3:FOR DOG AUDIO
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+frequency_sampling, audio_signal = wavfile.read("C:/Users/PMLS/Downloads/Audio_dog.wav")
+print('signal shape:', audio_signal.shape)
+print('signal Datatype:', audio_signal.dtype)
+print('signal Duration:', round(audio_signal.shape[0]/float (frequency_sampling),2),'seconds')
+audio_signal=audio_signal /np.power(2,15)
+audio_signal= audio_signal[:100]
+time_axis=1000*np.arange(0,len(audio_signal),1) / float(frequency_sampling)
+plt.plot(time_axis,audio_signal,color='yellow')
+plt.xlabel('Time(miliseconds)')
+plt.ylabel('Amplitude')
+plt.title('Input audio signal')
+#4:FOR HARVARD SOUND:
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+frequency_sampling, audio_signal = wavfile.read("C:/Users/PMLS/Downloads/harvard.wav")
+print('signal shape:', audio_signal.shape)
+print('signal Datatype:', audio_signal.dtype)
+print('signal Duration:', round(audio_signal.shape[0]/float (frequency_sampling),2),'seconds')
+audio_signal=audio_signal /np.power(2,15)
+audio_signal= audio_signal[:100]
+time_axis=1000*np.arange(0,len(audio_signal),1) / float(frequency_sampling)
+plt.plot(time_axis,audio_signal,color='yellow')
+plt.xlabel('Time(miliseconds)')
+plt.ylabel('Amplitude')
+plt.title('Input audio signal')
+plt.show()
+
+plt.show()
